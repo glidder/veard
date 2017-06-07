4d8b23ab678a8a94ae587d77f5c337d7b8817344
Luis J. Quintana <faceless.goblin@gmail.com>
2017-06-07 16:26:45 +0200

    Merging of REST classes under a single one

--- src/es/uca/veard/rest/Rest.java
+++ src/es/uca/veard/rest/Rest.java
@@ -25,13 +25,39 @@ public class Rest {
 		System.out.print("\nCACACACACACACACACACA\n"+ecode);
 		Pdao.save(name, desc, ecode, jcode);//Add a check function to the form!!!!!!
 	}
-
-  // This method is called if TEXT_PLAIN is requested
-  @GET
-  @Path("/hello")
-  @Produces(MediaType.TEXT_PLAIN)
-  public String sayPlainTextHello() {
-    return "Hello Jersey "+Rest.hum;
-  }
-
+    
+    // This method is called if TEXT_PLAIN is requested
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello() {
+        return "Hello Jersey "+Rest.hum;
+    }
+    
+    @GET
+	@Path("/list")
+	@Produces(MediaType.TEXT_HTML)
+	public String listProjects() {
+		List<String> projects = Pdao.listAll();
+		String result ="<ul  class='thumbnails'>";
+		if(!projects.isEmpty()){
+			for (String project:projects){
+				System.out.print(project);
+				result +=//"<li class='col-md-3'><div>"+name+"</div></li>";
+				"<li class='col-md-3'><div class='thumbnail'>"+
+	                "<img src='http://placehold.it/320x200' alt='ALT NAME'><div class='caption'>"+
+	                  "<h3>"+Pdao.getName(project)+"</h3>"+
+	                  "<p>"+Pdao.getDescription(project) +"</p>"+
+	                  "<p align='center'><a href='"+"viewer.html?proc="+project+"' class='btn btn-primary btn-block'>Open</a></p></div></div></li>";
+			}
+		}
+		return result;
+	}
+	
+	@GET
+	@Path("/project/{name}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getCode(@PathParam ("name") String name) {
+		return Pdao.load(name);
+	}
 } 
