e6875f45c44c15c4da96238ac9ae409d95963901
Luis Jose Quintana Bolaño <luis.quintanabolano@altran.es>
2017-06-13 13:01:24 +0200

    Adding log capabilities to the REST service

--- src/es/uca/veard/rest/Rest.java
+++ src/es/uca/veard/rest/Rest.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.annotation.PostConstruct;
-import javax.annotation.Config;
+import javax.annotation.*;
 
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
