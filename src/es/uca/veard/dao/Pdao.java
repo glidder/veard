068221f9a6bd193a16dc875a18ec6ea40c967177
Luis J. Quintana <faceless.goblin@gmail.com>
2017-06-29 11:00:48 +0200

    Implementing the REST methods

--- src/es/uca/veard/dao/Pdao.java
+++ src/es/uca/veard/dao/Pdao.java
@@ -172,7 +172,7 @@ public class Pdao {
 		try {
 			
             //Open the buffers
-			fw = new FileWriter(file.getAbsoluteFile(),false);
+			fw = new FileWriter(file.getAbsoluteFile());
 			bw = new BufferedWriter(fw);
             
             //Register the message in the log file
