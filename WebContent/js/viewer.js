195bc2ea9e4fa23c4d49ddd68fd809301fd221f9
VEARD <faceless.goblin@gmail.com>
2017-07-19 12:35:49 +0000

    testing viewer fix

--- WebContent/js/viewer.js
+++ WebContent/js/viewer.js
@@ -4,15 +4,37 @@
  * Copyright (c) Luis J. Quintana B.
  */
 $(document).ready(function(){
-	$.get( "./rest/dao/projects/"+$.urlParam('proc'), function( data ) {
-		$("#theviewer").load("editor_view.html",function () {  
-            setTimeout(function(){
-				loadCodeandRun(data);	
-			}, 1000);// cough cough
-            setTimeout(function(){
-				loadCodeandRun(data);	
-			}, 5000);
-			
-	    });
-	});
+     var project = $.urlParam('proc');
+     if (typeof(project) === 'string') {
+         $.get( "./rest/dao/projects/"+project, function( data ) {
+             $("#theviewer").load("editor_view.html",function () {
+                 setTimeout(function(){
+                     loadCodeandRun(data);
+                 }, 1000);// cough cough
+              });
+         });
+     }
 });
+//$(document).ready(function(){
+//	var code;
+	//var iframeView = $( "#theviewer" );//document.querySelector('iframe.viewFrame');
+//	$.get( "https://46.101.38.23:8443/veard/rest/dao/projects/"+$.urlParam('proc'), function( data ) {
+		//var iframeView		= document.querySelector('iframe.viewFrame');        
+		 //iframeView.contentWindow.init(); // TEMPORAL???? 		
+		//$('#theviewer')[0].contentWindow.loadCodeandRun(data);
+//		$("#theviewer").load("editor_view.html",function () {  
+//            setTimeout(function(){
+//				onLoad();
+//				loadCodeandRun(data);	
+//			}, 1000);// cough cough
+//            setTimeout(function(){
+//				loadCodeandRun(data);	
+//			}, 5000);
+//			
+//	    });
+//		code = data;
+//	});
+//	setTimeout(function(){
+//		loadCodeandRun(code);
+//	}, 5000);
+//});
